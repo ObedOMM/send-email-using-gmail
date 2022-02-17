afcc24a4c06ca902a38b5d4ca8d6a7145151d8d2
Obed Mulumba <ommartiste007@gmail.com>
2022-02-17 15:52:54 +0000

    Update EmailController.php

--- send-email-using-gmail/app/Http/Controllers/EmailController.php
+++ send-email-using-gmail/app/Http/Controllers/EmailController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
-    //
+        // ------------- [ Send email ] --------------------
+        public function sendEmailToUser() {
+
+            $to_email = "umesh.rana0269@gmail.com";
+    
+            Mail::to($to_email)->send(new MyEmail);
+    
+            return "<p> Your E-mail has been sent successfully. </p>";
+    
+        }
 }
